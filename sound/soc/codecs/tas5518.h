258ca90563d8e1e0c07b1dc8de3f3c2e183d15a0
Heiko Wilke <heiko.wilke@gmail.com>
2016-01-12 18:15:35 +0000

    - Add I2C init values for codec

--- sound/soc/codecs/tas5518.h
+++ sound/soc/codecs/tas5518.h
@@ -57,22 +57,39 @@ static const struct tas5518_init_command tas5518_init_sequence[] = {
         { .size = 2,  .data = "\x03\x90" },
         // Modulation Index Limit Register - Limit (DCLKS) = 5 & MIN WIDTH (DCLKS) = 10 & MODULATION INDEX = 96.1%
         { .size = 2,  .data = "\x16\x04" },
-        // Set Master Volume
-        { .size = 5,  .data = "\xD9\x00\x00\x00\x90" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD1\x00\x00\x00\x48" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD2\x00\x00\x00\x48" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD3\x00\x00\x00\x48" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD4\x00\x00\x00\x48" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD5\x00\x00\x00\x48" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD6\x00\x00\x00\x48" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD7\x00\x00\x00\x48" },
-        // Set Channel Volume to 0dB
-        { .size = 5,  .data = "\xD8\x00\x00\x00\x48" },
+        
+        /*
+        // Route Input Channel 1 to Output Channel 7
+        { .size = 33,  .data = "\x47\x00\x80\x00\x00\
+                                    \x00\x00\x00\x00\
+                                    \x00\x00\x00\x00\
+                                    \x00\x00\x00\x00\
+                                    \x00\x00\x00\x00\
+                                    \x00\x00\x00\x00\
+                                    \x00\x00\x00\x00\
+                                    \x00\x00\x00\x00" },
+        
+        // Channel 1 to Channel 8 Mixer
+        { .size = 5,  .data = "\x49\x00\x80\x00\x00" },       
+        // Channel 2 to Channel 8 Mixer
+        { .size = 5,  .data = "\x4A\x00\x80\x00\x00" },   
+        // Disable Channel 8 Input
+        { .size = 5,  .data = "\x50\x00\x00\x00\x00" }, 
+        // Set Channel 7 Output to Channel 8
+        { .size = 13,  .data = "\xB0\x70\x80\x00\x00\
+                                    \x00\x00\x00\x00\
+                                    \x00\x00\x00\x00" }, 
+        
+        // Biquad Filter 50 Hz Lowpass on Channel 8
+        { .size = 5,  .data = "\x84\x00\x00\x00\x69" },
+        // Biquad Filter 100 Hz Lowpass
+        { .size = 5,  .data = "\x85\x00\x00\x00\xD3" }, 
+        // Biquad Filter 100 Hz Lowpass
+        { .size = 5,  .data = "\x86\x00\x00\x00\x69" }, 
+        // Biquad Filter 100 Hz Lowpass
+        { .size = 5,  .data = "\x87\x00\xFE\xB5\xE1" }, 
+        // Biquad Filter 100 Hz Lowpass
+        { .size = 5,  .data = "\x88\xFF\x81\x48\x78" }, 
+
+        */
 };
